a3537424e76739867ac4974b330b469727564cb0
HaiHT <haiht369@gmail.com>
2023-10-18 11:18:02 +0700

    add custom port

--- next-frontend/ecosystem.config.js
+++ next-frontend/ecosystem.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    apps: [{
-      name: 'oohcoupons.com',
-      exec_mode: 'cluster',
-      instances: '1', // Or a number of instances
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-      env_local: {
-        APP_ENV: 'local' // APP_ENV=local
-      },
-      env_development: {
-        APP_ENV: 'dev' // APP_ENV=dev
-      },
-      env_production: {
-        APP_ENV: 'prod' // APP_ENV=prod
-      }
-    }]
-  }
+  apps: [{
+    name: 'oohcoupons.com',
+    exec_mode: 'cluster',
+    instances: '1', // Or a number of instances
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    env_local: {
+      APP_ENV: 'local', // APP_ENV=local
+      PORT: 3001
+    },
+    env_development: {
+      APP_ENV: 'dev', // APP_ENV=dev
+      PORT: 3001
+    },
+    env_production: {
+      APP_ENV: 'prod', // APP_ENV=prod
+      PORT: 3001
+    }
+  }]
+}
