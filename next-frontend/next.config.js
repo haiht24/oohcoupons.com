0a80d0448e3564c184bb2f38b3f7bff47f306f8b
Shuu09 <nghiemtrang0810@gmail.com>
2023-11-06 11:20:46 +0700

    update link for chrome and edge

--- next-frontend/next.config.js
+++ next-frontend/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
     env: {
         // Link to page install extension by browsers
         FIREFOX_EXT: '#',
-        CHROME_EXT: '#',
-        EDGE_EXT: '#',
+        CHROME_EXT: 'https://chrome.google.com/webstore/detail/oohcoupons-automatic-coup/dcifkfjlcblnhbpobekjhnfmnpcdojkd/related',
+        EDGE_EXT: 'https://microsoftedge.microsoft.com/addons/detail/oohcoupons-automatic-co/onmmocohcfnlohnoolaihphgpfhceokp',
     }
 }
 
